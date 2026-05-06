bf8db63a2f8804c39c3215bf21960adb25fde9e7
rosenoire <162016745+888Roses@users.noreply.github.com>
2026-05-06 13:55:59 +0200

    Updated todo

--- src/main/java/dev/rosenoire/regy/pipeline/AbstractRegy.java
+++ src/main/java/dev/rosenoire/regy/pipeline/AbstractRegy.java
@@ -21,20 +21,19 @@ public abstract class AbstractRegy {
     }
 
     /*
-     *  Expected Features:
+     *  TODO: Expected Features:
+     *  **Server Side:**
+     *      - <I extends Item> ItemEntryBuilder<I> item(String name, ItemFactory<I> factory)
+     *      - <B extends Block> BlockEntryBuilder<B> block(String name, BlockFactory<B> factory)
+     *      - <F extends Fluid> FluidEntryBuilder<F> fluid(String name, FluidFactory<F> factory)
+     *      - <E extends Entity> EntityEntryBuilder<E> entity(String name, EntityFactory<E> factory)
+     *      - SoundEntryBuilder sound(String name)
+     *      - CommandEntryBuilder command(String name, String root)
+     *      - HudElementEntryBuilder<H> <H extends HudElement> hudElement(String path, H instance)
+     *      - ParticleEntryBuilder<O> <O extends ParticleOptions> particle(String name, O options)
+     *      - AttributeEntryBuilder attribute(String name)
      *
-     *  Server Side:
-     *      <I extends Item> ItemEntryBuilder<I> item(String name, ItemFactory<I> factory)
-     *      <B extends Block> BlockEntryBuilder<B> block(String name, BlockFactory<B> factory)
-     *      <F extends Fluid> FluidEntryBuilder<F> fluid(String name, FluidFactory<F> factory)
-     *      <E extends Entity> EntityEntryBuilder<E> entity(String name, EntityFactory<E> factory)
-     *      SoundEntryBuilder sound(String name)
-     *      CommandEntryBuilder command(String name, String root)
-     *      HudElementEntryBuilder<H> <H extends HudElement> hudElement(String path, H instance)
-     *      ParticleEntryBuilder<O> <O extends ParticleOptions> particle(String name, O options)
-     *      AttributeEntryBuilder attribute(String name)
-     *
-     *  Client Side:
-     *      <O extends ParticleOptions> ClientParticleEntryBuilder<O> particle(ParticleEntry<O> entry)
+     *  **Client Side:**
+     *      - <O extends ParticleOptions> ClientParticleEntryBuilder<O> particle(ParticleEntry<O> entry)
      */
 }
